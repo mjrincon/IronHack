5e19c60e3019f5e3bd99f07cdca601877ec8e191
mjrincon <mathewjrincon@gmail.com>
2016-01-10 17:22:58 -0500

    First of day

---
+++ Pre-Work/shoppingcart.rb
@@ -0,0 +1,28 @@
+class ShoppingCart
+  def initialize (items)
+    @items = []
+  end
+end
+
+class items
+	def initialize (name, price)
+	@name = name
+	@price = price
+	end
+
+	def price
+		
+	end
+end 
+	
+class Houseware < Item 
+	def price
+		#hmm
+	end
+end
+
+class Fruit < Item
+	def price
+		#hmm again
+	end
+end					
