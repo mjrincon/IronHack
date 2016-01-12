d5bb4c77112615fad73c07c2a94d41b5b5287bea
mjrincon <mathewjrincon@gmail.com>
2016-01-12 11:58:13 -0500

    attr_read

---
+++ BootCamp/Week1/Cars/Person/lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+	attr_reader :name
+		def initialize(name, age)
+		@name = name
+		@age = age
+		end
+end
---
+++ BootCamp/Week1/Cars/Person/person_app.rb
@@ -0,0 +1,4 @@
+require_relative("lib/person.rb")
+
+person_1 = Person.new("Frank Underwood", 58)
+puts person_1.name
