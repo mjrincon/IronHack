853af565fd17d39b66c9c8b68c84bffa73516496
mjrincon <mathewjrincon@gmail.com>
2016-01-18 11:37:50 -0500

    String_Calculator

---
+++ BootCamp/Week2/Day6/iteration1/app.rb
@@ -0,0 +1,14 @@
+require_relative("lib/string_calculator")
+
+my_calc = StringCalculator.new
+
+p my_calc.add( "1,2" ) == 3
+p my_calc.add( "4,7" ) == 11
+p my_calc.add( "" ) == 0
+p my_calc.add( "9" ) == 9
+p my_calc.add( "5" ) == 5
+
+
+p "-------------------"
+
+
