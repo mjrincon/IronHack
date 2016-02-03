1da300b3ebd5b94a0008a91aa59212efe136179d
mjrincon <mathewjrincon@gmail.com>
2016-02-03 12:18:47 -0500

    Updated New View

--- timetracking/app/controllers/entries_controller.rb
+++ timetracking/app/controllers/entries_controller.rb
@@ -9,4 +9,25 @@ class EntriesController < ApplicationController
 		@project = Project.find(params[:project_id])
 		@entry = project.entries.new
 	end
+
+	def create
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.new params(entry_params)
+
+		if @entry.save
+			redirect_to_action: "index", controller: "entries", project_id: @project_id
+		else
+			render 'new'
+		end
+
+
+	end
+
+	private
+
+		def entry_params
+			params.requre(:entry).permit(:hours, :minutes, :date)
+		end
+
+
 end
--- timetracking/app/models/project.rb
+++ timetracking/app/models/project.rb
@@ -10,7 +10,4 @@ class Project < ActiveRecord::Base
 
 
 
-
-
-
 end
