fdb739a105ded76c6028dff0ae8f3194b1921b23
mjrincon <mathewjrincon@gmail.com>
2016-02-08 15:23:19 -0500

    Toggle sauce

--- BootCamp/Week5/pizza-builder-master/src/pizza.js
+++ BootCamp/Week5/pizza-builder-master/src/pizza.js
@@ -5,3 +5,7 @@ $('.btn-pepperonni').on('click', function () {$('.pep').toggle()});
 $('.btn-mushrooms').on('click', function () {$('.mushroom').fadeToggle()});
 
 $('.btn-green-peppers').on('click', function () {$('.green-pepper').slideToggle(1500)});
+
+
+$('.btn-sauce').on('click', function () {$('.sauce').toggleClass('sauce-white')});
+
