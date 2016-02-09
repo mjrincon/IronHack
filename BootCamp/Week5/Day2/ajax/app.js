a6a2ce880600455c219ea16418be56dc388e8a0a
mjrincon <mathewjrincon@gmail.com>
2016-02-09 11:43:14 -0500

    Add Character Button

--- BootCamp/Week5/Day2/ajax/app.js
+++ BootCamp/Week5/Day2/ajax/app.js
@@ -1,33 +1,89 @@
-$.ajax({
 
-url:"https://ironhack-characters.herokuapp.com/characters",
-
-success: function (response) {
-	console.log("Everything gonna be alright mahn");
-
-	displayCharacters(response);
-},
-
-error: function () {
-	console.log("WHAT ARE WE GONNA DOO?!?!");
-
-}
-});
-
-function displayCharacters (characters) {
-
-	characters.forEach(function (char) {
-
-		var html = `
-			<li>
-				<h2> ${char.name} </h2>
-				<p># ${char.id} </p>
-				<p>Occupation: ${char.occupation}</p>
-				<p>Weapon: ${char.weapon}</p>
-			</li>
-		`;
-
-		$(".js-characters-list").append(html);
+$(document).on("ready", function () {
+	$(".js-show-characters").on("click", function () {
+		fetchCharacters();
 	});
 
+	$(".js-add-jar").on("click", function () {
+		publishJarJar();
+
+
+	});
+
+
+
+});
+
+// DEFINITIONS
+// -------------------------------------------------
+
+
+function fetchCharacters () {
+
+	$.ajax({
+
+		url:"https://ironhack-characters.herokuapp.com/characters",
+
+		success: function (response) {
+			console.log("Everything gonna be alright mahn");
+
+			displayCharacters(response);
+		},
+
+		error: function () {
+			console.log("WHAT ARE WE GONNA DOO?!?!");
+
+		}
+	});
 }
+
+	function displayCharacters (characters) {
+
+		characters.forEach(function (char) {
+
+			var html = `
+				<li>
+					<h2> ${char.name} </h2>
+					<p># ${char.id} </p>
+					<p>Occupation: ${char.occupation}</p>
+					<p>Weapon: ${char.weapon}</p>
+				</li>
+			`;
+
+			$(".js-characters-list").append(html);
+		});
+
+	}
+
+
+function publishJarJar () {
+	var newCharacter= {
+		name:"Darth Jar Jar",
+		occupation:"Sith Lord",
+		weapon:"Lightsaber"
+	}
+	$.ajax({
+		type: "POST",
+
+		url: "https://ironhack-characters.herokuapp.com/characters",
+
+		data: newCharacter,
+
+		success: function () {
+			alert("Darth Jar Jar has been added succesfully");
+
+		},
+
+		error: function () {
+			alert("Your dark side power is not strong enough to add this character")
+		},
+	});
+}
+
+
+
+
+
+
+
+
