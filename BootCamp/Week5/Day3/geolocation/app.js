b826f877d99aa78560114b032dbfd767af11f31e
mjrincon <mathewjrincon@gmail.com>
2016-02-10 14:09:18 -0500

    Geolocation complete

--- BootCamp/Week5/Day3/geolocation/app.js
+++ BootCamp/Week5/Day3/geolocation/app.js
@@ -6,38 +6,43 @@ $(document).on("ready", function(){
 
 		getUserLocation();
 	}
-
 	else {
+		console.log("Browser sucks")
 		//Browser does not have geolocation
-		console.log("Computer sucks")
 	}
 
-});
+})
 
+//--------------------------------------------------------------------------------------
 
-
-
-
-
-function getUserLocation () {
-
-	navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-	//Optional 3rd parameter
+function getUserLocation() {
+	//Third parameter is an object of options
+	navigator.geolocation.getCurrentPosition(success, error);
 }
 
-function successCallback (position) {
-
-	console.log("Success!!!!!Q!WE@ER#");
+function success(position) {
+	console.log("Success");
 	console.log(position);
-		// console.log("Lat", position.coords.latitude);
-		// console.log("Long", position.coords.longitude);
 
 	$(".js-lat-value").text(position.coords.latitude);
+	$(".js-long-value").text(position.coords.longitude);
 
-	$(".js-lng-value").text(position.coords.longitude);
+	var latitude = position.coords.latitude
+	var longitude = position.coords.longitude
+
+	initMap(latitude, longitude);
 }
 
-function errorCallback(error) {
+function error(error) {
+	console.log("Error", error);
+}
 
-	console.log("Error, WHATS WRONG WITH YOU?!?!?", error);
+function initMap(latitude, longitude) {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: latitude, lng: longitude},
+    scrollwheel: false,
+    zoom: 19,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  });
 }
