7b5ab5b8f650cafa5b3c66cdabba7d02297a695d
mjrincon <mathewjrincon@gmail.com>
2016-02-15 17:56:21 -0500

    Added Player Model

--- BootCamp/Week6/Day1/video-games-master/app/models/tournament.rb
+++ BootCamp/Week6/Day1/video-games-master/app/models/tournament.rb
@@ -1,2 +1,5 @@
 class Tournament < ActiveRecord::Base
+
+	has_many :players
+
 end
--- BootCamp/Week6/Day1/video-games-master/db/schema.rb
+++ BootCamp/Week6/Day1/video-games-master/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215224242) do
+ActiveRecord::Schema.define(version: 20160215225515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tournaments", force: :cascade do |t|
     t.string   "name"
