024a620c14a7b9b89a396e107b33997137804253
mjrincon <mathewjrincon@gmail.com>
2016-02-16 11:00:22 -0500

    Login working

--- BootCamp/Week6/Day2/auth-exercise-master/app/controllers/application_controller.rb
+++ BootCamp/Week6/Day2/auth-exercise-master/app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-  end  
+
+  	if @current_user.nil?
+  		@current_user = User.find_by(id: session[:user_id])
+  	end
+
+  	@current_user
+	end
+
 end
