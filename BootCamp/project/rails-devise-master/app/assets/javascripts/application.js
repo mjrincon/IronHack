41d6b197cd85c9af961520161c4437dd22e334bf
mjrincon <mathewjrincon@gmail.com>
2016-02-27 16:08:26 -0500

    Changed Navbar to white

--- BootCamp/project/rails-devise-master/app/assets/javascripts/application.js
+++ BootCamp/project/rails-devise-master/app/assets/javascripts/application.js
@@ -27,3 +27,32 @@ $(function(){
         $('.card-reveal').slideToggle('slow');
     });
 });
+
+// VIDEO SHIT BELOW
+
+// var vid = document.getElementById("bgvid");
+// var pauseButton = document.querySelector("#polina button");
+
+// function vidFade() {
+//   vid.classList.add("stopfade");
+// }
+
+// vid.addEventListener('ended', function()
+// {
+// // only functional if "loop" is removed
+// vid.pause();
+// // to capture IE10
+// vidFade();
+// });
+
+
+// pauseButton.addEventListener("click", function() {
+//   vid.classList.toggle("stopfade");
+//   if (vid.paused) {
+//     vid.play();
+//     pauseButton.innerHTML = "Pause";
+//   } else {
+//     vid.pause();
+//     pauseButton.innerHTML = "Paused";
+//   }
+// })
